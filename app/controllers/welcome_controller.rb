2cb619d308e7117cb716ac46937b0a7943ceaddc
jessejones1948 <olathanbake1@yahoo.com>
2015-05-05 09:38:43 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homeland = "Georgia"
+  	@countries = ['Nigeria','France','Ghana']
+  	@images = ['naija.jpg','nigeria.jpg','france.jpg','ghana.jpg']
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size].to_i
+  end
+end
