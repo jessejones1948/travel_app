26d5a54d0fb92fce1b9e8f7f3df1a830231a1ee1
jessejones1948 <olathanbake1@yahoo.com>
2015-05-07 10:45:34 -0400

    updated files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
   	@homeland = "Georgia"
   	@countries = ['Nigeria','France','Ghana']
   	@images = ['naija.jpg','nigeria.jpg','france.jpg','ghana.jpg']
+
+  	@united_states = {"Capital city" => "Washington, DC", "Favorite City" => "Atlanta", "Favorite State" => "Georgia", "Flag Colors" => "Red,White & Blue"}
   end
 
   def about
